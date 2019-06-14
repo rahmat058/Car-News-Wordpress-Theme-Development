b55c20b88344781d91187e23428c4b8d20e8084a
rahmat058 <md.kazi.rahmat@gmail.com>
2019-06-14 22:47:02 +0600

    Home page is converted done

--- index.php
+++ index.php
@@ -26,12 +26,18 @@
     <link rel="icon" type="image/png" sizes="16x16" href="images/fav-icon/favicon-16x16.png">
     <!-- Master css File Here -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/master.css">
+
+		<!-- Custom Css File Here -->
+		<link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
+		
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+		<?php wp_head(); ?>
 </head>
 
 <body class="home">
@@ -690,6 +696,7 @@
     <!-- Custom JS File -->
     <script src="<?php echo get_template_directory_uri();?>/js/custom.js"></script>
 
+		<?php wp_footer(); ?>
 </body>
 
 </html>
